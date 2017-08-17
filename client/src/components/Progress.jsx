ca1e890337bcf97e3f5d881a718361a200cfaf10
Veselina Kolova <vmkolova@gmail.com>
2017-08-17 23:59:44 +0300

    Player changes

--- client/src/components/Progress.jsx
+++ client/src/components/Progress.jsx
@@ -5,8 +5,8 @@ import '../styles/Progress.scss'
 const Progress = props => (
     <div className='progress'>
         <div className='progress-bar'></div>
-        <div className='progress-val-bar'></div>
-        <div className='progress-val-pointer'></div>
+        <div className='progress-val-bar' style={{'width': `${(props.current/props.max) * 100}%`}}></div>
+        <div className='progress-val-pointer' style={{'left': `${(props.current/props.max) * 100}%`}}></div>
     </div>
 )
 
