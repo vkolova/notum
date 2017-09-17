5f984342723117be641c638e727d1735f9fd6a28
Veselina Kolova <vmkolova@gmail.com>
2017-09-17 23:03:28 +0300

    Added history navigation

--- client/src/components/shared/AppFrame.jsx
+++ client/src/components/shared/AppFrame.jsx
@@ -8,28 +8,44 @@ import Icon from './Icon'
 import '~~/styles/AppFrame.scss'
 
 class AppFrame extends Component {
+	constructor(props) {
+		super()
+		console.log(props)
+	}
+
 	render = () => (
     <div className='app-frame'>
-			<div
-				className='frame-btn'
-				onClick={ () => win.minimize() }
-			>
-        <Icon icon='window-minimize'/>
-      </div>
+			<div>
+				<span className='history-arrow' onClick={ () => window.history.back() }>
+					<Icon icon='chevron-left'/>
+				</span>
+				<span className='history-arrow' onClick={ () => window.history.forward() }>
+					<Icon icon='chevron-right'/>
+				</span>
+			</div>
 
-			<div
-				className='frame-btn'
-				onClick={ () => win.isMaximized() ? win.unmaximize() : win.maximize() }
-			>
-        <Icon icon={ win.isMaximized() ? 'window-restore' : 'window-maximize'}/>
-      </div>
+			<div className='frame-btns'>
+				<div
+					className='frame-btn'
+					onClick={ () => win.minimize() }
+					>
+					<Icon icon='window-minimize'/>
+				</div>
 
-      <div
-				className='frame-btn'
-				onClick={ () => win.close() }
-			>
-        <Icon icon='close'/>
-      </div>
+				<div
+					className='frame-btn'
+					onClick={ () => win.isMaximized() ? win.unmaximize() : win.maximize() }
+					>
+					<Icon icon={ win.isMaximized() ? 'window-restore' : 'window-maximize'}/>
+				</div>
+
+				<div
+					className='frame-btn'
+					onClick={ () => win.close() }
+					>
+					<Icon icon='close'/>
+				</div>
+			</div>
     </div>
   )
 }
