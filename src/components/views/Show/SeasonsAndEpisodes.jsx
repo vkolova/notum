535dd181eeaa7cd6a52ddeb5e31b1b427777f4ff
Veselina Kolova <vmkolova@gmail.com>
2017-10-14 21:54:13 +0300

    Season and episode selection :sunglasses:

--- src/components/views/Show/SeasonsAndEpisodes.jsx
+++ src/components/views/Show/SeasonsAndEpisodes.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { observer } from 'mobx-react'
-import { computed } from 'mobx'
+import { observable, computed } from 'mobx'
 
 import Icon from '../../shared/Icon'
 import setWindowTitle from '../../shared/window-title'
@@ -13,29 +13,47 @@ import showAPI from '~~/services/show'
 class SeasonsAndEpisodes extends Component {
   constructor(props) {
     super()
-    this.state = { season: 1, episode: 1 }
-    // this.activeSeason = computed(() => props.store.length > 0 ? props.store[this.state.season - 1].episodes : [])
-    // this.activeEpisode = computed(() => props.store.length > 0 ? this.activeSeason.episodes[this.state.episode - 1] : {})
+    this.state = observable({ season: 1, episode: 1 })
+    this.seasons = computed(() => 15)
+  }
+
+  setActiveSeason = index => {
+    this.state.season = index + 1
+    this.state.episode = 1
+  }
+
+  setActiveEpisode = index => {
+    this.state.episode = index + 1
   }
 
 	render = () => (
 			<div className='show-section display-row'>
         <div className='show-tab'>
           {
-            this.props.store.map(s => <div className='sm-item' key={s.id.toString()}>{s.name}</div>)
+            this.props.store.map((s, i) =>
+              <div className='sm-item' key={s.id.toString()} onClick={this.setActiveSeason.bind(this, i)}>{s.name}</div>
+            )
           }
         </div>
         <div className='show-tab'>
           {
             this.props.store.length > 0 &&
-              this.props.store[this.state.season - 1].episodes.map(s => <div className='sm-item' key={s.id.toString()}>{s.name}</div>)
+              this.props.store[this.state.season - 1].episodes
+                .map((s, i) =>
+                  <div className='sm-item' key={s.id.toString()} onClick={this.setActiveEpisode.bind(this, i)}>
+                    <span>{s.episode_number}. </span>
+                    {s.name}
+                  </div>
+                )
           }
         </div>
         <div className='show-tab'>
-          {
-            this.props.store.length > 0 &&
-              this.props.store[this.state.season - 1].episodes[this.state.episode - 1].overview
-          }
+          <p>
+            {
+              this.props.store.length > 0 && this.props.store[this.state.season - 1].episodes[this.state.episode - 1] &&
+                this.props.store[this.state.season - 1].episodes[this.state.episode - 1].overview
+            }
+          </p>
         </div>
 			</div>
 		)
