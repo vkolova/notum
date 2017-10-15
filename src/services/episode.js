7e2895ebd87c55d1e27c85d8d8e6c5c5595b2636
Veselina Kolova <vmkolova@gmail.com>
2017-10-15 10:10:09 +0300

    Clean up :metal:

--- src/services/episode.js
+++ src/services/episode.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 import UserStore from '~~/stores/user'
 import consts from '~~/constants'
 
-const markEpisodeAsWatched = ({episode, date = null}) =>
-	axios.put(`${consts.SERVER_URL}/episode`, {episode, date, token: UserStore.token})
+const markEpisodeAsWatched = ({show, episode, date = null}) =>
+	axios.put(`${consts.SERVER_URL}/episode`, {show, episode, date, token: UserStore.token})
 
 export default {
 	markEpisodeAsWatched
