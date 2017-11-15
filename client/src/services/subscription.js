fbbdacde98ab694cd9caebb4d5024c73805d380b
Veselina Kolova <vmkolova@gmail.com>
2017-11-15 21:51:20 +0200

    Front end stuff fixes :green_heart:

--- client/src/services/subscription.js
+++ client/src/services/subscription.js
@@ -3,9 +3,10 @@ import axios from 'axios'
 import UserStore from '~~/stores/user'
 import consts from '~~/constants'
 
+axios.defaults.headers.common['x-access-token'] = UserStore.token;
 
 const subscribe = data =>
-	axios.post(`${consts.SERVER_URL}/subscribe`, { ...data, token: UserStore.token })
+	axios.post(`${consts.SERVER_URL}/subscribe`, { ...data })
 
 
 export default {
