9a85076120a72a5e4f77c7d7569df6d204e09851
Veselina Kolova <vmkolova@gmail.com>
2017-11-16 18:39:22 +0200

    Heroku prod

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'server_secret': 'thisismyserversecret',
+	'app_secret': 'appsecretitis',
+	'database': 'mongodb://heroku_hz606vts:nn5iqidbaa71nb8pe1a3bcu6it@ds163561.mlab.com:63561/heroku_hz606vts'
+}
